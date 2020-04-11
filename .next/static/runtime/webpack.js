1ffed8f37d166f7aafbc5265d2463caf6abf5fdd
corbinarnett <corbin.c.arnett@gmail.com>
2020-04-11 16:35:08 -0700

    create index.js as starter file, add scripts

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "fea7d18173969ab7448b";
+/******/ 	var hotCurrentHash = "140092757acce3796c02";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
